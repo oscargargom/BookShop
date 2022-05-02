25e0f389cfd93a55fbfb3b78c042a0739711d00f
oscargargom <oscargargom001@gmail.com>
2022-05-02 17:05:08 +0200

    Frontend

--- login.php
+++ login.php
@@ -1,11 +1,3 @@
-
-<?php
-
-//Para que el css se vuelva a cargar, provisional
-header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
-header("Expires: Sat, 1 Jul 2000 05:00:00 GMT"); // Fecha en el pasado
-?>
-
 <?php
 include_once 'database.php';
 session_start();
