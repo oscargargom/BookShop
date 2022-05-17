41f9eaf3178a75160b6233d074d2eceb6c21c057
oscargargom <oscargargom001@gmail.com>
2022-05-17 21:53:33 +0200

    Cambios pequeños

--- shoppingcart/products.php
+++ shoppingcart/products.php
@@ -12,10 +12,10 @@ if (isset($_POST['add_to_cart'])) {
    $select_cart = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name'");
 
    if (mysqli_num_rows($select_cart) > 0) {
-      $message[] = 'product already added to cart';
+      $message[] = 'El libro ya ha sido añadido al carrito';
    } else {
       $insert_product = mysqli_query($conn, "INSERT INTO `cart`(name, price, image, quantity) VALUES('$product_name', '$product_price', '$product_image', '$product_quantity')");
-      $message[] = 'product added to cart succesfully';
+      $message[] = 'Libro añadido al carrito correctamente';
    }
 }
 
