4a65a25c4f5ebc4978acea419077f84f6f2db164
DESKTOP-KGRIBN7\Oscar <oscargargom001@gmail.com>
2022-05-29 20:15:42 +0200

    Mejoras en el login

--- login.php
+++ login.php
@@ -50,7 +50,8 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
 
 <head>
     <meta charset="utf-8">
-    <title>Login</title>
+    <title>Garden of books</title>
+    <link rel="icon" href="img/logo.png">
     <link rel="stylesheet" href="css/style.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" charset="utf-8"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
